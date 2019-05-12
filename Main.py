28b62d5cf03279e849167032b7642dabe5d9bf64
Benjamin <benjamin@Benjamins-MacBook-Pro.local>
2019-05-13 00:27:37 +0200

    starting using argparser

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 import time
 from tkinter import mainloop
-
+import argparse
 import _thread
 from ricochet_robot.GUI.GUI import Application
 from ricochet_robot.game.Ricochet import Ricochet
@@ -37,13 +37,47 @@ def actions(rico, app):
         app.lastLog.set(f"{step[0]} : {step[1]}")
         app.lastLog.set(f"Win ? {rico.isWin()}")
 
-def main():
-    rico = Ricochet()
-    rico.grid.loadGrid("grids/grid1.csv")
-    app = Application(board=rico.grid, showGrid=True)
+def show():
+    pass
 
-    _thread.start_new_thread( actions, (rico, app) )
-    app.mainloop()
+def learn():
+    pass
+
+def play():
+    pass
+
+def args():
+    parser = argparse.ArgumentParser(description="Reinforcement Ricochet Robot (3R)")
+    subparsers = parser.add_subparsers(help='Sub-commands you can use')
+
+    # command show
+    parser_show = subparsers.add_parser('show', help='Show a grid')
+    parser_show.set_defaults(func=show)
+
+    # command learn
+    parser_learn = subparsers.add_parser('learn', help='Learn a model')
+    parser_learn.add_argument('-i', '--input', help='A pretrained model')
+    parser_learn.add_argument('-o', '--output', help='The output model')
+    parser_learn.set_defaults(func=learn)
+
+    # command play
+    parser_play = subparsers.add_parser('play', help='Play a model')
+    parser_play.add_argument('model', help='The model you want to use')
+    parser_play.set_defaults(func=play)
+
+    parser.add_argument("grids", nargs="+", help="The grids you want to play on")
+    parser.add_argument("-v", "--verbose", action="store_true", help="Increase output verbosity")
+    return parser.parse_args()
+
+
+def main():
+    print(args().input)
+    # rico = Ricochet()
+    # rico.grid.loadGrid("grids/grid1.csv")
+    # app = Application(board=rico.grid, showGrid=True)
+
+    # _thread.start_new_thread( actions, (rico, app) )
+    # app.mainloop()
     
 
 if __name__ == "__main__":
